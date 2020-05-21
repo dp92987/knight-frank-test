3dc548fbb9efae1ee42dac9956e0b54600c2b075
Daniil <55208041+dp92987@users.noreply.github.com>
2020-05-22 02:49:20 +0300

    Add search form to catalog

--- knight_frank_test/database.py
+++ knight_frank_test/database.py
@@ -15,16 +15,20 @@ db_name = secret['db_name']
 conn = psycopg2.connect(dbname=db_name, host=db_host, user=db_user, password=db_password)
 
 
-def get_realties():
+def get_realties(area=None, floor=None, metro=None):
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     sql = '''
           SELECT DISTINCT r.id, r.name, r.area, r.type
           FROM realties as r
           LEFT JOIN realty_metros as rm on r.id=rm.realty_id
-          LEFT JOIN metros as m on rm.metro_id=m.id;
+          LEFT JOIN metros as m on rm.metro_id=m.id
+          WHERE (%(area)s is null or r.area=%(area)s)
+            AND (%(floor)s is null or r.floor=%(floor)s)
+            AND (%(metro)s is null or m.name ILIKE LOWER(%(metro)s));
           '''
+    kwargs = {'area': area, 'floor': floor, 'metro': f'%{metro}%' if metro else None}
     try:
-        cur.execute(sql)
+        cur.execute(sql, kwargs)
         query = cur.fetchall()
     except:
         conn.rollback()
--- knight_frank_test/routes.py
+++ knight_frank_test/routes.py
@@ -1,12 +1,25 @@
-from flask import render_template
+from flask import render_template, request
+from wtforms import Form, IntegerField, StringField, validators
 
 from .app import app
 from . import database as db
 
 
+class SearchForm(Form):
+    area = IntegerField('Площадь', validators=[validators.Optional(), validators.NumberRange(min=1, max=10000)])
+    floor = IntegerField('Этаж', validators=[validators.Optional(), validators.NumberRange(min=1, max=1000)])
+    metro = StringField('Найти', validators=[validators.Optional(), validators.Length(max=100)],
+                        filters=[lambda s: s or None])
+
+
 @app.route('/')
 @app.route('/catalog/')
 def catalog():
-    realties = db.get_realties()
-    response = render_template('catalog.html', realties=realties)
+    search_form = SearchForm(request.args)
+    if search_form.validate():
+        args = {element.id: element.data for element in search_form}
+    else:
+        args = {}
+    realties = db.get_realties(**args)
+    response = render_template('catalog.html', realties=realties, form=search_form)
     return response

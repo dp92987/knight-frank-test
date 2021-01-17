c53be27665a4ab35cd20416370886b7f29222347
dp92987 <55208041+dp92987@users.noreply.github.com>
2021-01-17 11:46:49 +0000

    Add monkey patching and psycogreen

--- knight_frank_test/__init__.py
+++ knight_frank_test/__init__.py
@@ -1,1 +1,10 @@
+import gevent
+import gevent.monkey
+
+gevent.monkey.patch_all()  # noqa
+
+import psycogreen.gevent
+
+psycogreen.gevent.patch_psycopg()
+
 from . import app, routes, database as db
